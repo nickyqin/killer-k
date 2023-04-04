9307ab56e7aa73139d1e652967ef55f1245256b1
sleepingsheeep <sheerawang1@gmail.com>
2023-04-03 21:48:39 -0500

    added models + attributes for the models

---
+++ Final_Project_Group18/Models/Interview.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+//TODO: Update this namespace to match your project
+namespace Final_Project_Group18.Models
+{ 
+    public class Interview
+    {
+        public Int32 InterviewID { get; set; }
+        // interviewer is the recruiter
+        [Display(Name = "Interviewer:")]
+        public AppUser Interviewer { get; set; }
+        public AppUser Student { get; set; }
+
+        [Display(Name = "Description:")]
+        public String Description { get; set; }
+        [Display(Name = "Interview Date:")]
+        public DateTime? InterviewDate { get; set; }
+
+        [Display(Name = "Time:")]
+        public String InterviewTime { get; set; }
+        [Display(Name = "Position:")]
+        public Position Position { get; set; }
+        [Display(Name = "Company:")]
+        public Company Company { get; set; }
+        [Display(Name = "Room:")]
+        public Int32 Room { get; set; }
+    }
+}
